ed4d167d5e990c8e442be01b8d12358f69a36fc4
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-17 23:19:07 +0100

    passo da js a nth-child... ora ho capito come si usano

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-function generateBox(outGrid, gridClass, borderClass, gridNum) {
+function generateBox(outGrid, gridClass, gridNum) {
   const square = document.createElement("div");
-  square.classList.add(gridClass, borderClass);
+  square.className=gridClass;
   outGrid.append(square);
   square.innerText = gridNum;
   square.addEventListener("click", function () {
@@ -43,65 +43,9 @@ function difficultyGame(selection) {
 function generateGrid() {
   grid.innerHTML = "";
   for (let i = 1; i <= gridNumBox; i++) {
-    if (gameBoxClass === "box_normal_100") {
-      if (i === 1) {
-        generateBox(grid, gameBoxClass, "border_top_left", i);
-      } else if (i <= 10) {
-        generateBox(grid, gameBoxClass, "border_top", i);
-      } else if (isInArray(i, myArray(100))) {
-        generateBox(grid, gameBoxClass, "border_left", i);
-      } else {
-        generateBox(grid, gameBoxClass, "default", i);
+        generateBox(grid, gameBoxClass, i);
       }
     }
-    if (gameBoxClass === "box_medium_81") {
-      if (i === 1) {
-        generateBox(grid, gameBoxClass, "border_top_left", i);
-      } else if (i <= 9) {
-        generateBox(grid, gameBoxClass, "border_top", i);
-      } else if (isInArray(i, myArray(81))) {
-        generateBox(grid, gameBoxClass, "border_left", i);
-      } else {
-        generateBox(grid, gameBoxClass, "default", i);
-      }
-    }
-    if (gameBoxClass === "box_hard_49") {
-      if (i === 1) {
-        generateBox(grid, gameBoxClass, "border_top_left", i);
-      } else if (i <= 7) {
-        generateBox(grid, gameBoxClass, "border_top", i);
-      } else if (isInArray(i, myArray(49))) {
-        generateBox(grid, gameBoxClass, "border_left", i);
-      } else {
-        generateBox(grid, gameBoxClass, "default", i);
-      }
-    }
-  }
-}
-function myArray(e) {
-  let arr = [];
-  if (e === 100) {
-    for (i = 1; i <= 100; i += 10) {
-      arr.push(i);
-    }
-  }
-  if (e === 81) {
-    for (i = 1; i <= 81; i += 9) {
-      arr.push(i);
-    }
-  }
-  if (e === 49) {
-    for (i = 1; i <= 49; i += 7) {
-      arr.push(i);
-    }
-  }
-  arr.shift();
-  return arr;
-}
-
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
-}
 function removeActiveClass() {
   const activeBtn = document.querySelector("button.active");
   activeBtn.classList.remove("active");
