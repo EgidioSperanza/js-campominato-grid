e3f0f51f4a809275d8fdd1b70508da48720a0ef8
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-17 16:34:56 +0100

    aggiunta classe attiva al bottone

--- js/main.js
+++ js/main.js
@@ -12,22 +12,26 @@ function difficultyGame(selection) {
   generateGrid();
   for (let i = 0; i < selection.childElementCount; i++) {
     selection.children.item(i).addEventListener("click", function () {
-      console.log(this.id);
+      removeActiveClass();
+      console.log(this.id); //DEBUG
       switch (this.id) {
         case "hard_difficulty":
           console.log("hard"); //DEBUG
           gridNumBox = 49;
           gameBoxClass = "box_hard_49";
+          this.className = "active";
           break;
         case "medium_difficulty":
           console.log("medium"); //DEBUG
           gridNumBox = 81;
           gameBoxClass = "box_medium_81";
+          this.className = "active";
           break;
         case "normal_difficulty":
           console.log("normal"); //DEBUG
           gridNumBox = 100;
           gameBoxClass = "box_normal_100";
+          this.className = "active";
           break;
         default:
       }
@@ -41,6 +45,11 @@ function generateGrid() {
   for (let i = 1; i <= gridNumBox; i++) {
     generateBox(grid, gameBoxClass, i);
   }
+}
+
+function removeActiveClass() {
+  const activeBtn = document.querySelector("button.active");
+  activeBtn.classList.remove("active");
 }
 
 const grid = document.getElementById("grid");
